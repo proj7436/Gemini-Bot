ab886103c316995ecbf0afb0377758c74cdb9551
proj7436 <144236533+proj7436@users.noreply.github.com>
2024-04-05 19:24:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ session_name_bot = "sessions/bot"
 
 # Follow this guide to obtain your API credentials: 
 # https://core.telegram.org/api/obtaining_api_id
-API_ID = ""
-API_HASH = ""
+API_ID = "28115400"
+API_HASH = "ee5b62e4c5ac96eb8de45c4c431b6ecb"
 
 # Write to @BotFather in Telegram to create a bot token
-BOT_TOKEN = ""
+BOT_TOKEN = "6525705295:AAGaoXer8twK9ZpvUDXyWVclGA3WD6ioH-0"
 
 # Configurations of vertexAi
-project_id = ""
-location = ""
+project_id = "peerless-kit-367813"
+location = "asia-east1"
